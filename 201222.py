058fb102b97e62c7a50cd8afb45d97b548f608ea
도기헌 <dogiheon@dogiheon-ui-MacBook-Pro.local>
2020-12-22 11:49:14 +0900

    201222

--- 201222.py
+++ 201222.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
+
 class myclass:
-    def __init__(self, *args):
+    def __init__(self, *args): #인스턴트 객체 자동생성
         self.var='hello'
         print('myclass : instant var is created')
 
