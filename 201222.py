9bdd28f977e160847f9148097d0d1eaa158432f4
도기헌 <dogiheon@dogiheon-ui-MacBook-Pro.local>
2020-12-22 15:28:56 +0900

    changed

--- 201222.py
+++ 201222.py
@@ -1,9 +1,18 @@
-# -*- coding: utf-8 -*-
+names={'mary':10999, 'sams':2111, 'aimy':9778, 'tom':20245, 'michale':27115, 'bob':5887, 'kelly':7885}
+ret1=sorted(names)
+print(ret1)
 
-class myclass:
-    def __init__(self, *args): #인스턴트 객체 자동생성
-        self.var='hello'
-        print('myclass : instant var is created')
+def f1(x):
+    return x[0]
 
-obj=myclass()
-print(obj.var)
+def f2(x):
+    return x[1]
+
+ret2=sorted(names.items(), key=f1)
+print(ret2)
+
+ret3=sorted(names.items(), key=f2)
+print(ret3)
+
+ret4=sorted(names.items(), key=f2, reverse=True)
+print(ret4)
