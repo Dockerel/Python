ce1b5eeb5e9b424741c73a16095e76964880b102
Giheon <74302278+Dockerel@users.noreply.github.com>
2021-01-07 22:28:46 +0900

    add fighter object

--- simple_Game/Galaga_game.py
+++ simple_Game/Galaga_game.py
@@ -6,12 +6,26 @@ import pygame
 BLACK = (0, 0, 0)
 pad_width = 480
 pad_height = 640
+fighter_width = 36
+fighter_height = 38
+
+# 게임에 등장하는 객체를 드로잉
+
+
+def drawObject(obj, x, y):
+    global gamepad
+    gamepad.blit(obj, (x, y))
 
 # 게임 실행 메인 함수
 
 
 def runGame():
-    global gamepad, clock
+    global gamepad, clock, fighter
+
+    # 전투기 초기 위치 (x,y)설정
+    x = pad_width*0.45
+    y = pad_height*0.9
+    x_change = 0
 
     ongame = False
     while not ongame:
@@ -19,8 +33,27 @@ def runGame():
             if event.type == pygame.QUIT:
                 ongame = True
 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change -= 5
+                elif event.key == pygame.K_RIGHT:
+                    x_change += 5
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_change = 0
+
         # 게임 화면을 검은색으로 채우고 화면을 업데이트 함
         gamepad.fill(BLACK)
+
+        # 전투기의 위치를 재조정
+        x += x_change
+        if x < 0:
+            x = 0
+        elif x > pad_width - fighter_width:
+            x = pad_width - fighter_width
+
+        drawObject(fighter, x, y)
         pygame.display.update()
         clock.tick(60)
 
@@ -30,12 +63,13 @@ def runGame():
 
 
 def initGame():
-    global gamepad, clock
+    global gamepad, clock, fighter
 
     pygame.init()
     gamepad = pygame.display.set_mode((pad_width, pad_height))
     clock = pygame.time.Clock()
     pygame.display.set_caption('MyGalaga')
+    fighter = pygame.image.load('fighter.png')
 
 
 initGame()
